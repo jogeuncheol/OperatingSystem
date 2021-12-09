571d6a128fdcb033150c074f8fcc8a02f12d7a24
당근철봉 <blueqwerty@naver.com>
2021-12-09 23:05:59 +0900

    [fix] ft_validtion.c [debug print]

--- dining_philosophers/ft_validation.c
+++ dining_philosophers/ft_validation.c
@@ -31,29 +31,29 @@ int	ft_arguments_validation(int argc, char *argv[])
 int	ft_sign_validation(t_data *share_data)
 {
 	if (share_data->nop <= 0)
-		return (printf("1\n"), 1);
+		return (1);
 	else if (share_data->ttd <= 0)
-		return (printf("2\n"), 1);
+		return (1);
 	else if (share_data->tte <= 0)
-		return (printf("3\n"), 1);
+		return (1);
 	else if (share_data->tts <= 0)
-		return (printf("4\n"), 1);
+		return (1);
 	else if ((long long)share_data->must_eat < -1)
-		return (printf("5\n"), 1);
+		return (1);
 	return (0);
 }
 
 int	ft_integer_validation(t_data *share_data)
 {
 	if (share_data->nop > 4294967295)
-		return (printf("6\n"), 1);
+		return (1);
 	else if (share_data->ttd > 4294967295)
-		return (printf("7\n"), 1);
+		return (1);
 	else if (share_data->tte > 4294967295)
-		return (printf("8\n"), 1);
+		return (1);
 	else if (share_data->tts > 4294967295)
-		return (printf("9\n"), 1);
+		return (1);
 	else if (share_data->must_eat > 4294967295)
-		return (printf("10\n"), 1);
+		return (1);
 	return (0);
 }
